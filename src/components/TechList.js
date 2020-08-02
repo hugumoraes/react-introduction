37edb173bf883178bff5824069b1283f728f1cf3
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2020-08-02 20:41:40 -0300

    Module complete

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
+import TechItem from './TechItem';
+
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   };
+
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -23,11 +35,17 @@ class TechList extends Component {
     });
   }
 
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => (<li key={tech}>{tech}</li>))}
+          {this.state.techs.map(tech => (
+            <TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} />
+          ))}
         </ul>
         <input type="text" onChange={this.handleInputChange} value={this.state.newTech} />
         <button type="submit">Enviar</button>
